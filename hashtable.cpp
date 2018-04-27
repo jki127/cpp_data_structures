6bc56629ce239ca0d78f86e07ff50766c3c24e53
Jayson Isaac <jayson.isaac1@gmail.com>
2018-04-27 11:27:28 -0400

    hashtable: add destructor

--- hashtable.cpp
+++ hashtable.cpp
@@ -24,9 +24,12 @@ class HashTable {
       slots = new list<Pair*>[size];
     }
     ~HashTable(){
-      // deallocate pairs
-      // deallocate lists
-      // deallocate slots
+      for(int i = 0; i < size; ++i){
+        for(Pair* current : slots[i]){
+          delete current;
+        }
+      }
+      delete[] slots;
     }
     void insert(const string& key, const string& value){
       int index = hash_func(key);
@@ -47,6 +50,19 @@ class HashTable {
 
 int main(){
   HashTable h;
+  h.insert("sunday", "0");
   h.insert("monday", "1");
+  h.insert("tuesday", "2");
+  h.insert("wednesday", "3");
+  h.insert("thursday", "4");
+  h.insert("friday", "5");
+  h.insert("saturday", "6");
+
+  cout << h["sunday"] << endl;
   cout << h["monday"] << endl;
+  cout << h["tuesday"] << endl;
+  cout << h["wednesday"] << endl;
+  cout << h["thursday"] << endl;
+  cout << h["friday"] << endl;
+  cout << h["saturday"] << endl;
 }
